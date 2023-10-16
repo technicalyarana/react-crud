03b59e85df6dcc81dc27522f8b64f721a01db256
Abhishek Maurya <technicalyarana@gmail.com>
2023-10-16 12:23:09 -0700

    Initial commit

---
+++ src/components/Employees.js
@@ -0,0 +1,18 @@
+ const Employees = [
+{
+id :"1",
+Name: "Abhishek Maurya",
+Age: "21"
+},{
+    id :"2",
+    Name: "Mohit Maurya",
+    Age: "25"
+    },
+    {
+        id :"3",
+        Name: "Sushil Maurya",
+        Age: "24"
+        },
+
+ ]
+ export default Employees;
